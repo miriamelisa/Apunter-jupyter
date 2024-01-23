3fd1af1fd0b2fc0273b1d91e76f0b73922bcae17
miriamelisa <miriam.guanolema1@gmail.com>
2024-01-23 19:33:04 +0100

    FLASKSQLITE

--- FLASK/aplicacion.py
+++ FLASK/aplicacion.py
@@ -1,33 +1,18 @@
-# Este es el lanzado
-from flask import Flask, render_template, jsonify
-
-app = Flask(__name__)  # vamos a hacer una aplicación de flask
-
-# Aquí se desarrolla la app ####################################
-
-
-# Vamos a hacer las rutas:
-@app.route("/")  # @= Quiere decir funcion decoradora, /= ruta principal
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
 def principal():
-    return render_template("principal.html")  # renderizar un archivo html
-
-
-# Otra ruta
-@app.route("/contactos")
+    return render_template('principal.html')
+ 
+ 
+@app.route('/contactos')
 def contactos():
-    return render_template("contactos.html")
-
-
-@app.route("/informacion", methods=["GET"])  # Saldrá lo que vamos a poner como API
-def obtenerifo():
-    return jsonify(
-        [
-            {"nombre": "Federico Rico", "telefono": "654211023"},
-            {"nombre": "Andrea", "telefono": "659844565"},
-        ]
-    )
-
-
-####################################
-if __name__ == "__main___":
+    return render_template('contactos.html')
+ 
+ 
+ 
+ 
+if __name__== '__main__':
     app.run(debug=True)
