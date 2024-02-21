b6caeaea0bfe5ae7a03af57817f9c32cca96034a
miriamelisa <miriam.guanolema1@gmail.com>
2024-02-21 17:40:47 +0100

    Pruebas y documentacion

---
+++ CLASES/DOCUMENTACIONYPRUEBAS/miModulo.py
@@ -0,0 +1,10 @@
+"""Módulo de prueba de la docuentación"""
+
+def hola():
+    """Saluda a quien la llame"""
+    print("¡Hola majetón!")
+
+
+def adios():
+    """Se despide de quien la llama"""
+    print("¡Chao Pescao!")
